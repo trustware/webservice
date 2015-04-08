a737cc4adaa1dd830803d4b3f7c7b8c45b05b2bf
Ben Doyle <bdoyle55@gmail.com>
2015-04-08 17:38:22 -0400

    spinner, /api, /demo, server side validation

---
+++ routes/api.js
@@ -0,0 +1,66 @@
+var express = require('express')
+    , morgan = require('morgan')
+    , memjs = require('memjs')
+    , async = require('async')
+    , crypto = require('crypto')
+    , path = require('path')
+    , bodyParser = require('body-parser')
+    , uuid = require('node-uuid')
+    , request = require('request')
+    , methodOverride = require('method-override')
+    , app = express()
+    , port = process.env.PORT || 3000
+    , router = express.Router();
+
+var client = memjs.Client.create('pub-memcache-10246.us-east-1-2.3.ec2.garantiadata.com:10246');
+
+
+router.post('/verify', function(req, res, next) { //URL for signup form to Post to
+	var email = req.body.email;
+	console.log('signup request received for: ' + email);
+	var token = req.body.token;
+	console.log('token: ' + token);
+	resp = 'nope';
+
+
+	client.set(token, '-1', function(err, success) {
+  	if (success) 
+  	{
+  		console.log('successfully set token with trust level: -1');
+
+  		var timerId = setInterval(function() {
+  			client.get(token, function (err, value, key) {
+		    	if (value != null) {
+			      	//console.log('current token trust level: ' + value.toString());
+			      	if (parseInt(value.toString()) > 50) {
+			      		resp = 'verified';
+			      		clearInterval(timerId);
+			      		res.send(resp);
+			      	}
+			      	else if (parseInt(value.toString()) != -1) {  //manufacturer says it's a bot
+			      		clearInterval(timerId); 
+			      		res.send(resp);
+			      	}
+		    	}	 
+		  	});
+  		},400);
+		}	
+		else {
+			console.log('DB error: ' + err)
+		}
+
+  });
+	
+});
+
+router.post('/devicecheck', function(req, res, next) { //URL for manufacturer to Post to
+	var trustlevel = req.body.trust,
+		token = req.body.token;
+	console.log('manufacturer response received for token ' + token + ' with trust level: ' + trustlevel);
+
+	client.set(token, trustlevel);
+	res.send('thanks');
+    
+});
+
+module.exports = router;
---
+++ routes/demo.js
@@ -0,0 +1,48 @@
+var express = require('express')
+    , morgan = require('morgan')
+    , memjs = require('memjs')
+    , async = require('async')
+    , crypto = require('crypto')
+    , path = require('path')
+    , bodyParser = require('body-parser')
+    , uuid = require('node-uuid')
+    , request = require('request')
+    , methodOverride = require('method-override')
+    , app = express()
+    , port = process.env.PORT || 3000
+    , router = express.Router();
+
+
+//stuff to be included on demo website
+
+router.get('/', function(req, res, next) {
+	var uuidtext = uuid.v4();
+    res.render('index', {token: uuidtext});
+});
+
+
+//singup form posts here, do the check to /api/verify (or the custom URL)
+router.post('/createaccount', function(req, res, next) { 
+	var emailtext = req.body.email , 
+		tokentext = req.body.trustwareInfo;
+
+	var verified = 'gotdevices?'
+
+	request.post('http://localhost:3000/api/verify', {form: {email: emailtext, token: tokentext}}, 
+		function (err, response, body) {
+  		verified = body;
+
+  		console.log('response: %s, error: %s', verified, err);
+  		
+	  	if (verified == 'verified') {
+				res.send('Congrats you have devices - account created'); 
+			} 
+			else {
+				res.send('Nice try robot!');
+			}
+	}); 
+
+});
+
+
+module.exports = router;
